609442e57751cf364fa17bd395a9d1c9074a302e
dimmerz92 <andrew.weymes@outlook.com>
2023-07-29 19:05:39 +0800

    add warning to board size selection

--- src/components/BoardSize.tsx
+++ src/components/BoardSize.tsx
@@ -1,5 +1,5 @@
 import styles from "./BoardSize.module.css";
-import { useContext } from "react";
+import { useContext, useState } from "react";
 import { SizeContext, UserContext } from "../contexts";
 import { Button, SizeButton } from ".";
 import { useNavigate } from "react-router-dom";
@@ -9,13 +9,23 @@ const TILES = [5, 7, 9, 11];
 export default function BoardSize() {
   const { size, newSize } = useContext(SizeContext);
   const { user } = useContext(UserContext);
+  const [unselected, setUnelected] = useState(false);
 
   const navigateTo = useNavigate();
   const handleSizeClick = (size: number) => newSize(size);
-  const handleSubmit = () => !user ? navigateTo("/login") : navigateTo("/game");
+  const handleSubmit = () => {
+    if (!size) {
+      setUnelected(true);
+    } else if (!user) {
+      navigateTo("/login");
+    } else {
+      navigateTo("/game");
+    }
+  }
 
   return (
     <div className={styles.sizeButtons}>
+      <span className={styles.unselected}>{unselected ? "Select a gameboard size" : null}</span>
       {TILES.map((tile) => (
           <SizeButton key={tile} size={tile} isSelected={tile===size?.size} onSelect={handleSizeClick} /> 
       ))}
