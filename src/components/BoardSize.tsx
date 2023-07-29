033123b0183dd44f142b489fec2b5ee95894364b
dimmerz92 <andrew.weymes@outlook.com>
2023-07-29 17:25:47 +0800

    add user context and complete boardsize component

--- src/components/BoardSize.tsx
+++ src/components/BoardSize.tsx
@@ -1,23 +1,25 @@
 import styles from "./BoardSize.module.css";
 import { useContext } from "react";
-import { SizeContext } from "../contexts";
+import { SizeContext, UserContext } from "../contexts";
 import { Button, SizeButton } from ".";
+import { useNavigate } from "react-router-dom";
 
 const TILES = [5, 7, 9, 11];
 
 export default function BoardSize() {
   const { size, newSize } = useContext(SizeContext);
+  const { user } = useContext(UserContext);
+
+  const navigateTo = useNavigate();
   const handleSizeClick = (size: number) => newSize(size);
-  const handleSubmit = () => console.log(size?.size);
+  const handleSubmit = () => !user ? navigateTo("/login") : navigateTo("/game");
 
   return (
-    <>
-      <div className={styles.sizeButtons}>
-        {TILES.map((tile) => (
-            <SizeButton key={tile} size={tile} isSelected={tile===size?.size} onSelect={handleSizeClick} /> 
-        ))}
-      </div>
+    <div className={styles.sizeButtons}>
+      {TILES.map((tile) => (
+          <SizeButton key={tile} size={tile} isSelected={tile===size?.size} onSelect={handleSizeClick} /> 
+      ))}
       <Button onClick={handleSubmit}>Start Game</Button>
-    </>
+    </div>
   )
 }
---
+++ src/providers/UserProvider.tsx
@@ -0,0 +1,19 @@
+import { User } from "../types";
+import { UserContext } from "../contexts";
+import { useState } from "react";
+
+type UserProviderProps = {
+    children: React.ReactNode;
+}
+
+export default function UserProvider({ children }: UserProviderProps) {
+    const [user, setUser] = useState<User | undefined>(undefined);
+    const login = (username: string) => setUser({ user: username });
+    const logout = () => setUser(undefined);
+
+    return (
+        <UserContext.Provider value={{user, login, logout}}>
+            { children }
+        </UserContext.Provider>
+    );
+}
