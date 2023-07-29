14e3f2b1e8f164d9910c6c8044bc36bceb67746b
dimmerz92 <andrew.weymes@outlook.com>
2023-07-29 17:59:18 +0800

    add login functionality

---
+++ src/components/Input.tsx
@@ -0,0 +1,7 @@
+import styles from "./Input.module.css"
+
+export default function Input(props: React.InputHTMLAttributes<HTMLInputElement>) {
+  return (
+    <input className={styles.input} type="text" autoComplete="false" {...props} />
+  )
+}
--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -1,5 +1,28 @@
+import styles from "./Login.module.css";
+import users from "../data/users.json";
+import { useState } from "react";
+import { Button, Input } from "../components";
+import { useNavigate } from "react-router-dom";
+
 export default function Login() {
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+  const [incorrect, setIncorrect] = useState(false);
+
+  const navigateTo = useNavigate();
+
+  const handleLogin = (e: React.MouseEvent) => {
+    e.preventDefault();
+    const user = users.find((u) => u.username === username && u.password === password);
+    !user ? setIncorrect(true) : navigateTo("/game");
+  }
+
   return (
-    <div>Login</div>
-  )
+    <form className={styles.form} onClick={handleLogin}>
+      <span className={styles.incorrect}>{incorrect ? "Incorrect username or password": null}</span>
+      <Input value={username} onChange={(e) => setUsername(e.target.value)} placeholder="Username" />
+      <Input type="password" value={password} onChange={(e) => setPassword(e.target.value)} placeholder="Password" />
+      <Button>Submit</Button>
+    </form>
+  );
 }
