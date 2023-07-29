8d09065a967806e0d910b7528caffbb77f78d476
dimmerz92 <andrew.weymes@outlook.com>
2023-07-29 15:35:33 +0800

    add router and test

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,22 @@
 import './App.css';
+import { Route, Routes } from 'react-router-dom';
+import { Home, Login, Game, Games, GameLog } from "./pages";
+import { Header } from './components';
 
 export default function App() {
   return (
-    <p>
-      Hello World
-    </p>
+    <>
+      <Header />
+      <main>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/game" element={<Game />} />
+          <Route path="/games" element={<Games />} />
+          <Route path="/game-log/:id" element={<GameLog />} />
+          <Route path="*" element={<Home />} />
+        </Routes>
+      </main>
+    </>
   );
 }
