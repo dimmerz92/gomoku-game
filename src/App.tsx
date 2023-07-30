954ccfb7c0e2b63aeb643b9cda989b3417a868f9
dimmerz92 <andrew.weymes@outlook.com>
2023-07-30 17:33:52 +0800

    test

--- src/App.tsx
+++ src/App.tsx
@@ -2,21 +2,28 @@ import './App.css';
 import { Route, Routes } from 'react-router-dom';
 import { Home, Login, Game, Games, GameLog } from "./pages";
 import { Header } from './components';
+import { GameboardProvider, SizeProvider, TurnProvider, UserProvider } from './providers';
 
 export default function App() {
   return (
-    <>
-      <Header />
-      <main>
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/login" element={<Login />} />
-          <Route path="/game" element={<Game />} />
-          <Route path="/games" element={<Games />} />
-          <Route path="/game-log/:id" element={<GameLog />} />
-          <Route path="*" element={<Home />} />
-        </Routes>
-      </main>
-    </>
+    <SizeProvider>
+      <TurnProvider>
+        <GameboardProvider>
+          <UserProvider>
+            <Header />
+            <main id="main">
+              <Routes>
+                <Route path="/" element={<Home />} />
+                <Route path="/login" element={<Login />} />
+                <Route path="/game" element={<Game />} />
+                <Route path="/games" element={<Games />} />
+                <Route path="/game-log/:id" element={<GameLog />} />
+                <Route path="*" element={<Home />} />
+              </Routes>
+            </main>
+          </UserProvider>
+        </GameboardProvider>
+      </TurnProvider>
+    </SizeProvider>
   );
 }
