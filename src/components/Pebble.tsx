6227aad08fd359ebdcee6f594dc114f101b13f95
dimmerz92 <andrew.weymes@outlook.com>
2023-07-30 16:33:45 +0800

    add turn handling

--- src/components/Pebble.tsx
+++ src/components/Pebble.tsx
@@ -1,15 +1,20 @@
 import styles from "./Pebble.module.css";
 import { PlayerColour } from "../constants";
+import { useState } from "react";
 
 type PebbleProps = {
   colour: PlayerColour
 }
 
 export default function Pebble({ colour }: PebbleProps) {
+  const [colourStyle, setColourStyle] = useState("");
 
   const getStyles = () => {
-    const pebbleColour = colour === PlayerColour.BLACK ? styles.black : styles.white;
-    return [styles.pebble, pebbleColour].join(" ");
+    if (!colourStyle) {
+      const pebbleColour = colour === PlayerColour.BLACK ? styles.black : styles.white;
+      setColourStyle([styles.pebble, pebbleColour].join(" "));
+    }
+    return colourStyle;
   }
 
   return (
