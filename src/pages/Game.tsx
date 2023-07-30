58c65838f9d5e4751abadf226f76d5bee12034a5
dimmerz92 <andrew.weymes@outlook.com>
2023-07-30 18:25:13 +0800

    add functional leave button

--- src/pages/Game.tsx
+++ src/pages/Game.tsx
@@ -2,13 +2,21 @@ import styles from "./Game.module.css";
 import { Button, Gameboard } from "../components";
 import { useContext, useState } from "react";
 import { GameboardContext } from "../contexts";
+import { GameStatus } from "../constants";
+import { useNavigate } from "react-router-dom";
 
 export default function Game() {
-  const { newBoard } = useContext(GameboardContext);
+  const { status, newBoard } = useContext(GameboardContext);
   const [isReset, setIsReset] = useState(0);
+  const navigateTo = useNavigate();
+  
   const reset = () => {
     newBoard();
     isReset ? setIsReset(0) : setIsReset(1);
+  }
+
+  const leave = () => {
+    navigateTo(status === GameStatus.NOT_OVER ? "/home" : "/games")
   }
 
   return (
@@ -16,7 +24,7 @@ export default function Game() {
       <Gameboard key={isReset} />
       <div className={styles.buttons}>
         <Button onClick={() => reset()}>Reset</Button>
-        <Button>Leave</Button>
+        <Button onClick={() => leave()}>Leave</Button>
       </div>
     </>
   )
