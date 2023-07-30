fa1e8a649fdb2ac7983d8a1e7a370f480c849098
dimmerz92 <andrew.weymes@outlook.com>
2023-07-30 16:35:42 +0800

    add inits

--- src/components/BoardSize.tsx
+++ src/components/BoardSize.tsx
@@ -1,6 +1,6 @@
 import styles from "./BoardSize.module.css";
 import { useContext, useState } from "react";
-import { SizeContext, UserContext } from "../contexts";
+import { GameboardContext, SizeContext, TurnContext, UserContext } from "../contexts";
 import { Button, SizeButton } from ".";
 import { useNavigate } from "react-router-dom";
 
@@ -8,15 +8,23 @@ const TILES = [5, 7, 9, 11];
 
 export default function BoardSize() {
   const { size, newSize } = useContext(SizeContext);
+  const { initTurn } = useContext(TurnContext);
+  const { newBoard } = useContext(GameboardContext);
   const { user } = useContext(UserContext);
-  const [unselected, setUnelected] = useState(false);
+  const [unselected, setUnselected] = useState(false);
 
   const navigateTo = useNavigate();
   const handleSizeClick = (size: number) => newSize(size);
   const handleSubmit = () => {
     if (!size) {
-      setUnelected(true);
-    } else if (!user) {
+      setUnselected(true);
+      return;
+    }
+
+    initTurn();
+    newBoard();
+    
+    if (!user) {
       navigateTo("/login");
     } else {
       navigateTo("/game");
@@ -27,9 +35,9 @@ export default function BoardSize() {
     <div className={styles.sizeButtons}>
       <span className={styles.unselected}>{unselected ? "Select a gameboard size" : null}</span>
       {TILES.map((tile) => (
-          <SizeButton key={tile} size={tile} isSelected={tile===size?.size} onSelect={handleSizeClick} /> 
+          <SizeButton key={tile} size={tile} isSelected={tile===size?.size} onSelect={() => handleSizeClick(tile)} /> 
       ))}
-      <Button onClick={handleSubmit}>Start Game</Button>
+      <Button onClick={() => handleSubmit()}>Start Game</Button>
     </div>
   )
 }
