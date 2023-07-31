692e10da4cced04bcb9ff2f0cfccf80e38688398
dimmerz92 <andrew.weymes@outlook.com>
2023-07-31 21:16:12 +0800

    add next turn in use effect hook

--- src/components/Pebble.tsx
+++ src/components/Pebble.tsx
@@ -1,23 +1,21 @@
 import styles from "./Pebble.module.css";
 import { GameStatus, PlayerColour } from "../constants";
-import { useContext, useState } from "react";
+import { useContext, useEffect, useState } from "react";
 import { GameboardContext, TurnContext } from "../contexts";
 
 export default function Pebble() {
-  const { status } = useContext(GameboardContext);
   const { turn, nextTurn } = useContext(TurnContext);
   const [colourStyle, setColourStyle] = useState("");
 
-  const getStyles = () => {
+  useEffect(() =>{
     if (!colourStyle) {
       const pebbleColour = turn!.turn === PlayerColour.BLACK ? styles.black : styles.white;
-      setColourStyle([styles.pebble, pebbleColour].join(" "));
-      if (status === GameStatus.NOT_OVER) nextTurn();
+      setColourStyle(`${styles.pebble} ${pebbleColour}`);
+      nextTurn();
     }
-    return colourStyle;
-  }
+  });
 
   return (
-    <div className={getStyles()} />
+    <div className={colourStyle} />
   )
 }
