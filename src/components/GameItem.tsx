5a1f657da9c0a0b3df819bf9661eb6ffb58ec599
dimmerz92 <andrew.weymes@outlook.com>
2023-08-01 22:50:33 +0800

    implement Games page and GameItem component

--- src/components/GameItem.tsx
+++ src/components/GameItem.tsx
@@ -1,11 +1,21 @@
+import styles from "./GameItem.module.css";
 import { Game } from "../types"
+import Button from "./Button";
 
 type GameItemProps = {
   game: Game;
 }
 
+const handleClick = () => {
+  // use game id to open game log
+}
+
 export default function GameItem({ game }: GameItemProps) {
   return (
-    <div>{`Game #${game.id} @${game.date}\tWinner ${game.outcome}`}</div>
+    <div className={styles.gameItem}>
+      <div>{`Game #${game.id} @${game.date}`}</div>
+      <div>{`Outcome: ${game.outcome}`}</div>
+      <Button>Game Log</Button>
+    </div>
   );
 }
--- src/pages/Games.tsx
+++ src/pages/Games.tsx
@@ -1,3 +1,4 @@
+import styles from "./Games.module.css";
 import { useContext } from "react";
 import { GameboardContext } from "../contexts";
 import { GameItem } from "../components";
@@ -6,10 +7,10 @@ export default function Login() {
   const { getGames } = useContext(GameboardContext);
 
   return (
-    <>
+    <div className={styles.container}>
       {getGames().map((game, i) => (
         <GameItem key={i} game={game} />
       ))}
-    </>
+    </div>
   )
 }
