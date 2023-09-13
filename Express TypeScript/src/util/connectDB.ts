557e77021938190cf247e2d7bca175b482f65c57
Andrew <andrew.weymes@outlook.com>
2023-09-13 20:24:37 +0800

    add connection to mongoDB

---
+++ Express TypeScript/src/util/connectDB.ts
@@ -0,0 +1,15 @@
+import mongoose from "mongoose";
+
+const connectDB = async() => {
+    const dbURI = process.env.dbURI || "";
+    console.log("[server]: Connecting to MongoDB Server");
+    try {
+        await mongoose.connect(dbURI);
+    } catch (error) {
+        console.log("[server]: Could not establish connection to database");
+        console.log(error);
+        process.exit(1);
+    }
+}
+
+export default connectDB;
