0dc1824796de514c04057f80ba544dd3ea1cc54a
Andrew <andrew.weymes@outlook.com>
2023-09-14 12:11:09 +0800

    refactor updateGameState and add comments

--- Express TypeScript/src/service/game.service.ts
+++ Express TypeScript/src/service/game.service.ts
@@ -1,12 +1,14 @@
 import mongoose from "mongoose";
 import GameModel, { GameInput, GameDocument } from "../model/game.model";
 
+// Retrieves a list of games belonging to a user
 export async function getGamesById(user_id: string) {
     return GameModel.find({
         user_id: new mongoose.Types.ObjectId(user_id)
     }).lean();
 }
 
+// Retrieves a game by it's ID, ensuring it belongs to the requesting user
 export async function getGameById(user_id: string, game_id: string) {
     return GameModel.findOne({
         _id: new mongoose.Types.ObjectId(game_id),
@@ -14,14 +16,29 @@ export async function getGameById(user_id: string, game_id: string) {
     }).lean();
 }
 
+// Updates the game state for a game ID and it's user
 export async function updateGameState
-    (user_id: string, game_id: string, input: GameDocument) {
+    (user_id: string, game_id: string, index: number, colour: string) {
+        const current_state = await getGameById(user_id, game_id);
+        if (!current_state) {
+            return null
+        }
+
+        const move = {
+            user_id: new mongoose.Types.ObjectId(user_id),
+            colour: colour
+        }
+
+        const new_state = current_state.gameboard.map(
+            (v, i) => i === index ? move : v);
+
         return GameModel.findOneAndUpdate({
             _id: new mongoose.Types.ObjectId(game_id),
             user_id: new mongoose.Types.ObjectId(user_id)
-        }, input, { new: true });
+        }, { gameboard: new_state }, { new: true });
 }
 
+// Creates a new game and assigns it to the user ID
 export async function createGame(input: GameInput) {
     const game = {
         user_id: new mongoose.Types.ObjectId(input.user_id),
