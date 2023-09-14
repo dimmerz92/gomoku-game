0e9010ca9a6d89317940b183c1c47dec8039c9f9
Andrew <andrew.weymes@outlook.com>
2023-09-14 12:16:54 +0800

    complete handlers

--- Express TypeScript/src/handler/game.handler.ts
+++ Express TypeScript/src/handler/game.handler.ts
@@ -2,40 +2,57 @@ import express, { Request, Response } from "express";
 import validateSchema from "../middleware/validateSchema";
 import { createGameSchema, updateGameSchema, getGameSchema } from
     "../schema/game.schema";
+import { createGame, getGameById, updateGameState } from "../service/game.service";
+import mongoose from "mongoose";
  
 
 const gameHandler = express.Router();
 
-// new game
+// Creates a new game
 gameHandler.post("/",
     validateSchema(createGameSchema),
     (req: Request, res: Response) => {
-        // create new game
-        // add to database
-        // return gamestate and id to client
-        console.log(req.body)
-        res.status(200).json({"hi": "hello"})
-    });
+        const user_id = new mongoose.Types.ObjectId(req.user_id);
+        const size = req.body.size;
 
-// take turn
+        const new_game = createGame({ user_id, size });
+        if (!new_game) {
+            return res.status(400);
+        }
+    
+        return res.status(201).json(new_game);
+});
+
+// Updates the game state and returns the new game state
 gameHandler.put("/id/:game_id",
     validateSchema(updateGameSchema),
     (req: Request, res: Response) => {
-        // read and validate the payload
-        // retrieve gamestate from database
-        // modify gamestate with players move
-        // save back in database
-        // send new gamestate back to client
-        console.log(req.body)
-        res.status(200).json({"that": "worked"})
-    });
+        const user_id = req.user_id;
+        const game_id = req.params.game_id;
+        const index = req.body.index;
+        const colour = req.body.colour;
 
+        const game_state = updateGameState(user_id, game_id, index, colour);
+        if (!game_state) {
+            return res.status(400);
+        }
+
+        return res.status(200);
+});
+
+// Returns a game
 gameHandler.get("/id/:game_id",
     validateSchema(getGameSchema),
     (req: Request, res: Response) => {
-        // get the game state from the data base
-        // send to client
-        res.status(200).json({"here": "you go"})
-    })
+        const user_id = req.user_id;
+        const game_id = req.params.game_id;
+
+        const game = getGameById(user_id, game_id);
+        if (!game) {
+            return res.status(400);
+        }
+
+        return res.status(200).json(game);
+});
 
 export default gameHandler;
