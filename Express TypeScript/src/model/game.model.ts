7bb4f5f5b29fada1196fe6fb3ac655fc3f9eee23
Andrew <andrew.weymes@outlook.com>
2023-09-14 10:51:26 +0800

    update game model

--- Express TypeScript/src/model/game.model.ts
+++ Express TypeScript/src/model/game.model.ts
@@ -1,30 +1,46 @@
 import mongoose, { Document } from "mongoose";
 import { UserDocument } from "./user.model";
 
-export interface GameDocument extends Document {
+export interface GameInput {
     user_id: UserDocument["_id"];
     size: number;
+}
+
+export interface GameDocument extends GameInput, Document {
     gameboard: Array<undefined | {
-        user_id: number;
+        user_id: UserDocument["_id"];
         colour: string;
     }>;
+    createdAt?: Date;
+    updatedAt?: Date;
 }
 
 const cellSchema = new mongoose.Schema({
     user_id: {
         type: mongoose.Schema.Types.ObjectId,
-        ref: "User"
+        ref: "User",
+        require: true
     },
-    colour: String
+    colour: {
+        type: String,
+        require: true
+    }
 });
 
 const gameSchema = new mongoose.Schema({
     user_id: {
         type: mongoose.Schema.Types.ObjectId,
-        ref: "User"
+        ref: "User",
+        require: true
     },
-    size: Number,
-    gameboard: [cellSchema]
-});
+    size: {
+        type: Number,
+        require: true
+    },
+    gameboard: {
+        type: [cellSchema],
+        require: true
+    }
+}, {timestamps: true});
 
 export default mongoose.model<GameDocument>("Game", gameSchema);
