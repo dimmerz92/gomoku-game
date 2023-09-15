3876dd3e709169c80fdfff976e4cccf462f6ef76
Andrew <andrew.weymes@outlook.com>
2023-09-15 18:28:22 +0800

    update request to create new game on express

--- React TypeScript/src/components/BoardSize.tsx
+++ React TypeScript/src/components/BoardSize.tsx
@@ -1,44 +1,32 @@
 import styles from "./BoardSize.module.css";
 import { useContext, useState } from "react";
-import {
-  GameboardContext,
-  SizeContext,
-  TurnContext,
-  UserContext,
-} from "../contexts";
+import { GameboardContext, UserContext } from "../contexts";
 import { Button, SizeButton } from ".";
 import { useNavigate } from "react-router-dom";
 
 const TILES = [5, 7, 9, 11];
 
 export default function BoardSize() {
-  const { size, newSize } = useContext(SizeContext);
-  const { initTurn } = useContext(TurnContext);
   const { newBoard } = useContext(GameboardContext);
   const { user } = useContext(UserContext);
-  const [unselected, setUnselected] = useState(false);
+  const [size, setSize] = useState<number | undefined>(undefined);
+  const [unselected, setUnselected] = useState<boolean>(false);
   const navigateTo = useNavigate();
-
-  // Handle clicks on size tiles
-  const handleSizeClick = (size: number) => newSize(size);
 
   // Handle clicks on the submit button
   const handleSubmit = () => {
-    if (!size) {
-      // if nothing selected, return error prompting
-      setUnselected(true);
-      return;
-    }
+    // If nothing selected, show error message
+    if (!size) setUnselected(true);
 
     // Route to Game page or reroute to login if not logged in
     if (!user) {
       navigateTo("/login");
     } else {
-      initTurn();
-      newBoard();
-      navigateTo("/game");
+      newBoard(size!, () => {
+        navigateTo("/game");
+      });
     }
-  };
+  }
 
   return (
     <div className={styles.sizeButtons}>
@@ -46,11 +34,8 @@ export default function BoardSize() {
         {unselected ? "Select a gameboard size" : null}
       </span>
       {TILES.map((tile) => (
-        <SizeButton
-          key={tile}
-          size={tile}
-          isSelected={tile === size?.size}
-          onSelect={() => handleSizeClick(tile)}
+        <SizeButton key={tile} size={tile} selected={tile === size}
+          onSelect={() => setSize(tile)}
         />
       ))}
       <Button onClick={() => handleSubmit()}>Start Game</Button>
