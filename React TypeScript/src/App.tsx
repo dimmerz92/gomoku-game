562739bd4b26e42bd69736a2179143b3ea69a30a
Andrew <andrew.weymes@outlook.com>
2023-09-16 16:27:46 +0800

    integrate express server

--- React TypeScript/src/App.tsx
+++ React TypeScript/src/App.tsx
@@ -2,27 +2,23 @@ import "./App.css";
 import { Route, Routes } from "react-router-dom";
 import { Home, Register, Login, Game, Games, GameLog } from "./pages";
 import { Header } from "./components";
-import { SizeProvider, TurnProvider, UserProvider } from "./providers";
+import { UserProvider } from "./providers";
 
 export default function App() {
   return (
-    <SizeProvider>
-      <TurnProvider>
-        <UserProvider>
-          <Header />
-          <main id="main">
-            <Routes>
-              <Route path="/" element={<Home />} />
-              <Route path="/register" element={<Register />} />
-              <Route path="/login" element={<Login />} />
-              <Route path="/game" element={<Game />} />
-              <Route path="/games" element={<Games />} />
-              <Route path="/game-log/:gameid" element={<GameLog />} />
-              <Route path="*" element={<Home />} />
-            </Routes>
-          </main>
-        </UserProvider>
-      </TurnProvider>
-    </SizeProvider>
+    <UserProvider>
+      <Header />
+      <main id="main">
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/register" element={<Register />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/game" element={<Game />} />
+          <Route path="/games" element={<Games />} />
+          <Route path="/game-log/:game_id" element={<GameLog />} />
+          <Route path="*" element={<Home />} />
+        </Routes>
+      </main>
+    </UserProvider>
   );
 }
